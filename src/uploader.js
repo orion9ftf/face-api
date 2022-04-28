40d6673723bf2a899d8f32ce57eaa2ab257f1f3b
eorion <brpmdr@mailfence.com>
2022-04-28 19:32:18 -0400

    uploadFile target file

--- src/uploader.js
+++ src/uploader.js
@@ -1,14 +1,18 @@
 //carga de archivos
 import { v4 as uuidv4 } from 'uuid'; //id del usuario
 
+window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+
 const uploadFile = file => {
-  window.requestFileSystem(window.TEMPORARY, 1024 * 1024, function(fs) {
-    fs.root.getFile(`${file.name}${uuidv4()}`, { create: true, exclusive: true }, function(fileEntry) {
-      fileEntry.createWriter(function(fileWriter) {
-        fileWriter.write(file);
-        console.log(fileEntry)
+  return new Promise((resolve, reject) => {
+    window.requestFileSystem(window.TEMPORARY, 1024 * 1024, function(fs) {
+      fs.root.getFile(`${file.name}${uuidv4()}`, { create: true, exclusive: true }, function(fileEntry) {
+          fileEntry.createWriter(function(fileWriter) {
+              fileWriter.write(file);
+              resolve(fileEntry);
+          }, e => console.log(e));
       }, e => console.log(e));
-    }, e => console.log(e));
+    })
   })
 }
 
@@ -16,9 +20,9 @@ const uploader = (submitSelector, imagesListSelector)=> {
   const submit = document.querySelector(submitSelector);
   const imagesList = document.querySelector(imagesListSelector);
 
-  submit.addEventListener('change', e => {
+  submit.addEventListener('change', async e => {
     //console.log('change funciona!')
-    console.log(e.target.files[0]);
+    console.log(await uploadFile(e.target.files[0]));
   })
   //eventos
 }
