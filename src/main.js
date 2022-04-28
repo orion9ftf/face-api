fe0353530de3d74bb2311ea1081f011395038bcf
eorion <brpmdr@mailfence.com>
2022-04-28 18:42:52 -0400

    add const draw face

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import 'babel-polyfill';
 
 //promesas
 const main = async ()=> {
-  const videocontainer = document.querySelector('.js-video');
+  const videoContainer = document.querySelector('.js-video');
   const canvas = document.querySelector('.js-canvas');
   const context = canvas.getContext('2d');
   const video = await navigator.mediaDevices.getUserMedia({video: true}); //pedir permiso a la cámara
-  
+
+  videoContainer.srcObject = video;
+  const reDraw = async () => {
+    context.drawImage(videoContainer, 0, 0, 640, 480);//nos permite dibujar el contexto(rostro)
+    requestAnimationFrame(reDraw);
+  }
+  requestAnimationFrame(reDraw);
 }
 
 main();
