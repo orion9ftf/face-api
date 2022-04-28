dec4261fb681685e0c4425859bb595b9613ffb4a
eorion <brpmdr@mailfence.com>
2022-04-28 17:54:20 -0400

    const and babel

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('hola!!');
+
+import 'babel-polyfill';
+
+//promesas
+const main = async ()=> {
+  const videocontainer = document.querySelector('.js-video');
+  const canvas = document.querySelector('.js-canvas');
+  const context = canvas.getContext('2d');
+}
+
+main();
