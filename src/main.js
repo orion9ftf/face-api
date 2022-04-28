19e33c0de6802cda7e06621ceb43b67fffc86d9e
eorion <brpmdr@mailfence.com>
2022-04-28 18:32:42 -0400

    permisos del navegador

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ const main = async ()=> {
   const videocontainer = document.querySelector('.js-video');
   const canvas = document.querySelector('.js-canvas');
   const context = canvas.getContext('2d');
+  const video = await navigator.mediaDevices.getUserMedia({video: true}); //pedir permiso a la cámara
+  
 }
 
 main();
